fdd93834a0de0ac9a7f31cca0aad3a29bd0ae1f5
Rui2o2o <ruislan@github.com>
2023-06-11 20:47:45 +0800

    upgrade fastify

--- ktap-ui-web/src/pages/footer.js
+++ ktap-ui-web/src/pages/footer.js
@@ -18,7 +18,7 @@ function Footer() {
                             })
                         }
                     }}>
-                        <RouterLink href='#' kind='underline' role='a' target='_blank'>Github</RouterLink>
+                        <RouterLink href='https://github.com/ruislan/ktap' kind='underline' role='a' target='_blank'>Github</RouterLink>
                         <RouterLink href='#' kind='underline' role='a' target='_blank'>博客</RouterLink>
                         <RouterLink href='/about' kind='underline' target='_blank'>关于我们</RouterLink>
                         <RouterLink href='/terms' kind='underline' target='_blank'>服务协议</RouterLink>
