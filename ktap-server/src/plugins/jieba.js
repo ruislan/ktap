73d1f6aa458c1635f29c28fac606e41270a23b93
Ruislan <z17520@126.com>
2023-09-06 14:55:28 +0800

    replace nodejieba to @node-rs/jieba

--- ktap-server/src/plugins/jieba.js
+++ ktap-server/src/plugins/jieba.js
@@ -1,13 +1,13 @@
 'use strict';
 import fp from 'fastify-plugin';
-import jieba from 'nodejieba';
+import jieba from '@node-rs/jieba';
 
 /*
 * 结巴分词的Plugin
 */
 const jiebaPlugin = async (fastify, opts, next) => {
     jieba.load();
-    fastify.decorate('jieba', { extract: jieba.extract, cut: jieba.cut, tag: jieba.tag, textRankExtract: jieba.textRankExtract });
+    fastify.decorate('jieba', { extract: jieba.extract, cut: jieba.cut, tag: jieba.tag });
     next();
 };
 
