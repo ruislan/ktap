558a1c8d6d91006c9a9b794dd5fc19dad273ead3
Ruislan <z17520@126.com>
2023-09-26 18:36:49 +0800

    add node event error handler

--- ktap-server/src/plugins/pubsub.js
+++ ktap-server/src/plugins/pubsub.js
@@ -1,15 +1,16 @@
 'use strict';
 import fp from 'fastify-plugin';
-import EventEmitter from 'events';
+import { EventEmitter } from 'node:events';
 
 const pubsub = {
-    provider: new EventEmitter(),
+    provider: new EventEmitter({ captureRejections: true }),
     publish: async (event, data) => pubsub.provider.emit(event, data),
     on: async (event, callback) => pubsub.provider.on(event, callback),
     removeAllListeners: async (event) => pubsub.provider.removeAllListeners(event),
 };
 
 const pubsubPlugin = async (fastify, opts, next) => {
+    pubsub.on('error', (err) => fastify.log.error(err)); // add error handler, callback method should use async
     fastify.decorate('pubsub', pubsub);
     fastify.addHook('onClose', async (fastify) => {
         fastify.log.info('waiting for pubsub to complete...');
